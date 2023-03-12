22971eabd832f42746f7105b60ba6965351b0114
Thanamond <thanapoom.boat@kmutt.ac.th>
2023-03-12 15:51:11 +0700

    task 6

--- Program.cs
+++ Program.cs
@@ -6,21 +6,43 @@ namespace practice1
     {
         static void Main(string[] args)
         {
-          Console.Write(" Input max number range : ");
-          float max = float.Parse(Console.ReadLine());
-          Console.Write(" Input min number range : ");
-          float min = float.Parse(Console.ReadLine());
-
-          Console.Write(" Input your number : ");
-          float num = float.Parse(Console.ReadLine());
-
-          if(num <= max && num >= min)
+          Console.Write("Please input a number of stations: ");
+          int n = int.Parse(Console.ReadLine());
+          if(n == 1)
           {
-            Console.WriteLine("True");
+            Console.Write("Cost (THB): 16 ");
           }
-          else
+          else if(n == 2)
           {
-            Console.WriteLine("False");
+            Console.Write("Cost (THB): 23 ");
+          }
+          else if(n == 3)
+          {
+            Console.Write("Cost (THB): 26 ");
+          }
+          else if(n == 4)
+          {
+            Console.Write("Cost (THB): 30 ");
+          }
+          else if(n == 5)
+          {
+            Console.Write("Cost (THB): 33 ");
+          }
+          else if(n == 6)
+          {
+            Console.Write("Cost (THB): 37 ");
+          }
+          else if(n == 7)
+          {
+            Console.Write("Cost (THB): 40 ");
+          }
+          else if(n >= 8 && n <= 15)
+          {
+            Console.Write("Cost (THB): 44 ");
+          }
+          else if(n > 15)
+          {
+            Console.Write("Cost (THB): 59 ");
           }
         }   
     }
