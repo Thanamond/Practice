0aecadfe1c46a5398bf8a2fc37ffd85cb12f4355
Thanamond <thanapoom.boat@kmutt.ac.th>
2023-03-12 16:19:55 +0700

    task 3

--- Program.cs
+++ Program.cs
@@ -6,44 +6,16 @@ namespace practice1
     {
         static void Main(string[] args)
         {
-          Console.Write("Please input a number of stations: ");
-          int n = int.Parse(Console.ReadLine());
-          if(n == 1)
-          {
-            Console.Write("Cost (THB): 16 ");
-          }
-          else if(n == 2)
-          {
-            Console.Write("Cost (THB): 23 ");
-          }
-          else if(n == 3)
-          {
-            Console.Write("Cost (THB): 26 ");
-          }
-          else if(n == 4)
-          {
-            Console.Write("Cost (THB): 30 ");
-          }
-          else if(n == 5)
-          {
-            Console.Write("Cost (THB): 33 ");
-          }
-          else if(n == 6)
-          {
-            Console.Write("Cost (THB): 37 ");
-          }
-          else if(n == 7)
-          {
-            Console.Write("Cost (THB): 40 ");
-          }
-          else if(n >= 8 && n <= 15)
-          {
-            Console.Write("Cost (THB): 44 ");
-          }
-          else if(n > 15)
-          {
-            Console.Write("Cost (THB): 59 ");
-          }
+          Console.Write(" Input the multiplier : ");
+          int mult = int.Parse(Console.ReadLine());
+            int i = 1;
+            int sum = 2;
+            while (i < mult)
+            {
+                sum *= 2;
+                i++;
+            }
+            Console.WriteLine("The result is : {0}",sum);
         }   
     }
 }
