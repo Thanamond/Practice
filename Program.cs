b183b90f2b54ea7c0d9890d20a2be8522d9a2210
Thanamond <thanapoom.boat@kmutt.ac.th>
2023-03-12 17:49:19 +0700

    task 11

--- Program.cs
+++ Program.cs
@@ -6,39 +6,11 @@ namespace practice1
     {
         static void Main(string[] args)
         {
-          Console.Write(" Input number :");
-          int number = int.Parse(Console.ReadLine());
+          int x = int.Parse(Console.ReadLine());
 
-          switch (number)
+          for (int i = 1; i <= x; i++)
           {
-            case 1:
-              Console.WriteLine("yellow");
-              break;
-            case 2:
-              Console.WriteLine("orange");
-              break;
-            case 3:
-              Console.WriteLine("yellow");
-              Console.WriteLine("orange");
-              break;
-            case 4:
-              Console.WriteLine("red");
-              break;
-            case 5:
-              Console.WriteLine("yellow");
-              Console.WriteLine("red");
-              break;
-            case 6:
-              Console.WriteLine("orange");
-              Console.WriteLine("red");
-              break;
-            case 7:
-              Console.WriteLine("yellow");
-              Console.WriteLine("orange");
-              Console.WriteLine("red");
-              break;
-            default:
-              break;
+            Console.WriteLine("Hamlet");
           }
         }  
     }
